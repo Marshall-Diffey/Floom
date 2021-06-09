dd7571d3b3e6df4c9917af748ae006a11e778d50
Marshall-Diffey <marshalldiffey@yahoo.com>
2021-06-09 10:26:48 -0500

    Add account creation

--- app/api/account_routes.py
+++ app/api/account_routes.py
@@ -1,15 +1,42 @@
 from flask import Blueprint, jsonify, request, session
+from flask_login import current_user
 from app.models import db, User, Account, Transaction
+from app.forms import AccountForm
 
 account_routes = Blueprint('accounts', __name__)
 
 
-@account_routes.route('/<int:id>')
-def get_accounts(id):
-    accounts = Account.query.filter_by(user_id = id).all()
+def validation_errors_to_error_messages(validation_errors):
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
+
+@account_routes.route('')
+def get_accounts():
+    accounts = current_user.accounts
+    # accounts = Account.query.filter_by(user_id = id).all()
     print('------------')
     print(accounts)
     # return {'accounts': accountList}
     return {'accounts': [
         account.to_simple_dict() for account in accounts
     ]}
+
+@account_routes.route('', methods=['POST'])
+def create_account():
+    form = AccountForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        account = Account(
+            name = form.data['name'],
+            amount = form.data['amount'],
+            user_id = form.data['user_id'],
+        )
+        db.session.add(account)
+        db.session.commit()
+        return account.to_dict()
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}
--- react-app/src/components/Accounts.js
+++ react-app/src/components/Accounts.js
@@ -1,10 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import Modal from 'react-modal';
+import AccountForm from './forms/Accounts'
+import { getAccounts } from '../store/account';
+
+const customStyles = {
+    content : {
+        top                   : '30%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)',
+        padding: 0,
+        // background: 'none',
+        // height: '100%',
+    }
+}
+
+Modal.setAppElement('body');
+
 
 const Accounts = () => {
     const [accountView, setAccountView] = useState(false);
-    const [accounts, setAccounts] = useState([]);
+    // const [accounts, setAccounts] = useState([]);
+    const [accountDisplayId, setAccountDisplayId] = useState(null);
+    const [accountDisplay, setAccountDisplay] = useState({});
+    const [createAccount, setCreateAccount] = useState(false);
+    // const [editAccount, setEditAccount] = useState(false);
     const user = useSelector(state => state.session.user);
+    const accounts = useSelector(state => Object.values(state.account))
+    const dispatch = useDispatch();
+    console.log(accountDisplay);
+
+    const openCreateAccount = () => {
+        setCreateAccount(true)
+    }
+
+    const closeCreateAccount = () => {
+        setCreateAccount(false)
+    }
 
     const accountsDisplay = () => {
         if (!accountView) {
@@ -16,12 +51,24 @@ const Accounts = () => {
     }
 
     useEffect(() => {
-        (async () => {
-            const res = await fetch(`/api/accounts/${user.id}`);
-            const data = await res.json();
-            setAccounts(data.accounts)
-        }) ()
+        // (async () => {
+        //     const res = await fetch(`/api/accounts/${user.id}`);
+        //     const data = await res.json();
+        //     setAccounts(data.accounts)
+        // }) ()
+        dispatch(getAccounts())
     }, [accountView])
+
+    useEffect(() => {
+        if (accounts) {
+            console.log(accounts)
+            for(const i in accounts) {
+                if(accounts[i].id === accountDisplayId) {
+                    return setAccountDisplay(accounts[i]);
+                }
+            }
+        }
+    }, [accountDisplayId])
 
     return (
         <div className="profilePage__accounts">
@@ -34,15 +81,24 @@ const Accounts = () => {
             {accountView ? "Close" : "View"}
             </button>
             {accountView? accounts.map((account) => (
-                <div key={account.id}>
+                <button key={account.id} value={account.id} onClick={e => setAccountDisplayId(e.target.value)}>
                     {account.name}
                     {/* <div>{account.amount}</div> */}
-                </div>
+                </button>
             ))
             : null}
             {accountView?
                 <div>
-                    <button>+ Add Bank Account</button>
+                    <button onClick={openCreateAccount}>+ Add Bank Account</button>
+                    <Modal
+                        isOpen={createAccount}
+                        onRequestClose={closeCreateAccount}
+                        style={customStyles}
+                        id="loginModal"
+                        // className="loginModal"
+                    >
+                        <AccountForm setCreateAccount={setCreateAccount}></AccountForm>
+                    </Modal>
                 </div>
             : null}
         </div>
